5d6cbaf753ffdd90f347a8b220c35bfc3e29d97f
thapaliyaprakash <holmsnick34@gmail.com>
2025-06-14 16:46:15 -0900

    Personal Website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [RouterModule]
 })
 export class App {
   protected title = 'firstweb';
